3c1f4cbd0fd240d36d47519b9963553d265e9730
HanielUlises <hanielulises2003@gmail.com>
2026-03-15 23:47:12 -0600

    Updated parthood relations

--- Basic/Parthood.lean
+++ Basic/Parthood.lean
@@ -19,15 +19,16 @@ def PartOf (x y : World) : Prop :=
 
 notation:50 x " ⊴ " y => PartOf x y
 
-theorem partOf_refl : ∀ x : World, x ⊴ x :=
-  fun _ _ h => h
+theorem partOf_refl (x : World) : x ⊴ x := by
+  intro F h
+  exact h
 
-theorem partOf_trans :
-    ∀ x y z : World, x ⊴ y → y ⊴ z → x ⊴ z :=
-  fun _ _ _ hxy hyz F hxF => hyz F (hxy F hxF)
+theorem partOf_trans (x y z : World) (hxy : x ⊴ y) (hyz : y ⊴ z) : x ⊴ z := by
+  intro F hxF
+  exact hyz F (hxy F hxF)
 
-theorem partOf_antisymm :
-    ∀ x y : World,
-      x ⊴ y → y ⊴ x →
-      ∀ F : Property, Enc x F ↔ Enc y F :=
-  fun _ _ hxy hyx F => ⟨fun h => hxy F h, fun h => hyx F h⟩
+theorem partOf_antisymm (x y : World) (hxy : x ⊴ y) (hyx : y ⊴ x) (F : Property) :
+    Enc x F ↔ Enc y F := by
+  constructor
+  · intro h; exact hxy F h
+  · intro h; exact hyx F h
